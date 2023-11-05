3bbeb98f44e701eedd8094f374b4b3b298fd442d
pavan <123@>
2023-11-05 17:34:46 +0530

    Prac11_5b6_75

---
+++ app/src/main/java/com/pavanpatel/mad11_5b6_75/MapsActivity.kt
@@ -0,0 +1,49 @@
+package com.pavanpatel.mad11_5b6_75
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.example.mad11_5b6_75.databinding.ActivityMapsBinding
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap      
+    private lateinit var binding: ActivityMapsBinding       
+    private val TAG = "MapActivity"      
+    private var lat = -34.0     
+    private var log = 151.0
+    private var title = "Marker in Sydney"
+
+    override fun onCreate(savedInstanceState: Bundle?) {        
+        super.onCreate(savedInstanceState)
+    }          val obj = intent.getSerializableExtra("Object") as Person          
+    Log.i(TAG, "onCreate: Object:$obj")          
+    lat = obj.Latitude          
+    log = obj.Longitude
+    title = obj.Name
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)          
+    setContentView(binding.root)
+
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.          
+    // val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment          
+    mapFragment.getMapAsync(this)
+    }
+        override fun onMapReady(googleMap: GoogleMap) {
+    mMap = googleMap
+
+    // Add a marker in Sydney and move the camera
+    // val sydney = LatLng(lat, log)
+    mMap.addMarker(MarkerOptions().position(sydney).title(title))
+        //
+// mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+// mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 8.0f))
+}
+
+
+
